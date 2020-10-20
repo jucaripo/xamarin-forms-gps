76efd8ebbe964916a504f64a3cd5e92c31eaee25
Juan Carlos Ricalde Poveda <jucaripo@hotmail.com>
2020-10-19 19:36:49 -0500

    * .gitignore: Inicio de proyecto
    * gps.sln:
    * App.xaml:
    * gps.csproj:
    * App.xaml.cs:
    * Main.cs:
    * AppShell.xaml:
    * Item.cs:
    * Info.plist:
    * AssemblyInfo.cs:
    * AppShell.xaml.cs:
    * GettingStarted.txt:
    * gps.iOS.csproj:
    * AppDelegate.cs:
    * ItemsPage.xaml:
    * LoginPage.xaml:
    * AboutPage.xaml:
    * IDataStore.cs:
    * Entitlements.plist:
    * NewItemPage.xaml:
    * AboutPage.xaml.cs:
    * MainActivity.cs:
    * ItemsPage.xaml.cs:
    * LoginPage.xaml.cs:
    * NewItemPage.xaml.cs:
    * Default.png:
    * ItemDetailPage.xaml:
    * MockDataStore.cs:
    * gps.Android.csproj:
    * tab_feed.png:
    * BaseViewModel.cs:
    * tab_about.png:
    * Default@2x.png:
    * AboutViewModel.cs:
    * ItemsViewModel.cs:
    * ItemDetailPage.xaml.cs:
    * LoginViewModel.cs:
    * tab_feed@2x.png:
    * tab_feed@3x.png:
    * tab_about@3x.png:
    * tab_about@2x.png:
    * NewItemViewModel.cs:
    * AssemblyInfo.cs:
    * xamarin_logo.png:
    * AboutAssets.txt:
    * xamarin_logo@2x.png:
    * xamarin_logo@3x.png:
    * Default-568h@2x.png:
    * ItemDetailViewModel.cs:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * colors.xml:
    * Tabbar.xml:
    * styles.xml:
    * AboutResources.txt:
    * Toolbar.xml:
    * Default-Portrait@2x.png:
    * LaunchScreen.storyboard:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * AndroidManifest.xml:
    * tab_feed.png:
    * icon.png:
    * tab_about.png:
    * icon.png:
    * icon.png:
    * xamarin_logo.png:
    * icon.xml:
    * Icon40.png:
    * Icon29.png:
    * Icon60.png:
    * Icon76.png:
    * Icon20.png:
    * Icon58.png:
    * Icon87.png:
    * Icon80.png:
    * icon_round.xml:
    * Icon180.png:
    * Icon167.png:
    * Icon120.png:
    * Icon152.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ gps/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using gps.Models;
+using gps.ViewModels;
+
+namespace gps.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
