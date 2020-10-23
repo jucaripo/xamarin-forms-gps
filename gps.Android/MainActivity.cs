60712016484c7005c8bd8fcfa4c53a5bf1caa909
Juan Carlos Ricalde Poveda <jucaripo@hotmail.com>
2020-10-23 17:22:58 -0500

    * App.xaml: * AppShell.xaml: * Info.plist: * gps.iOS.csproj: * MainActivity.cs: * GeoCodingPage.xaml: * GeoGeneralPage.xaml: * gps.Android.csproj: * GeoCodingPage.xaml.cs: * AssemblyInfo.cs: * AndroidManifest.xml:
    * GeolocalizacionPage.xaml: GeoCoding

--- gps.Android/MainActivity.cs
+++ gps.Android/MainActivity.cs
@@ -20,6 +20,7 @@ namespace gps.Droid
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
--- gps/Views/GeoCodingPage.xaml.cs
+++ gps/Views/GeoCodingPage.xaml.cs
@@ -1,15 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
+using System.Threading;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace gps.Views
 {
     public partial class GeoCodingPage : ContentPage
     {
+        CancellationTokenSource cts;
+
         public GeoCodingPage()
         {
             InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+
+            try
+            {
+
+                var request = new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromSeconds(10));
+                // obtenemos el token de cancelación
+                cts = new CancellationTokenSource();
+                // y pasamos a pedir que el celular calcule la ubicación 
+                var location = await Geolocation.GetLocationAsync(request, cts.Token);
+                // el objeto location nos entrega los valores para trabajar con ellos.
+
+
+                if (location != null)
+                {
+
+
+
+                    var placemarks = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
+
+                    var placemark = placemarks?.FirstOrDefault();
+                    if (placemark != null)
+                    {
+                        txtAdminArea.Text = placemark.AdminArea;
+                        txtCountryCode.Text = placemark.CountryCode;
+                        txtCountryName.Text = placemark.CountryName;
+                        txtFeatureName.Text = placemark.FeatureName;
+                        txtLocality.Text    = placemark.Locality;
+                        txtPostalCode.Text  = placemark.PostalCode;
+                        txtSubAdminArea.Text = placemark.SubAdminArea;
+                        txtSubLocality.Text = placemark.SubLocality;
+                        txtSubThoroughfare.Text = placemark.SubThoroughfare;
+                        txtThoroughfare.Text = placemark.Thoroughfare;
+
+
+                        var geocodeAddress =
+                            $"AdminArea:       {placemark.AdminArea}\n" +
+                            $"CountryCode:     {placemark.CountryCode}\n" +
+                            $"CountryName:     {placemark.CountryName}\n" +
+                            $"FeatureName:     {placemark.FeatureName}\n" +
+                            $"Locality:        {placemark.Locality}\n" +
+                            $"PostalCode:      {placemark.PostalCode}\n" +
+                            $"SubAdminArea:    {placemark.SubAdminArea}\n" +
+                            $"SubLocality:     {placemark.SubLocality}\n" +
+                            $"SubThoroughfare: {placemark.SubThoroughfare}\n" +
+                            $"Thoroughfare:    {placemark.Thoroughfare}\n";
+
+                        Console.WriteLine(geocodeAddress);
+                    }
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Handle exception that may have occurred in geocoding
+            }
+
         }
     }
 }
